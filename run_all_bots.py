0170d792de069c9697c4659d8da72138620b7693
ALEKSANDRO ALVES DA ROCHA JUNIOR <97574073+aajunior43@users.noreply.github.com>
2023-07-17 19:14:27 -0300

    Update run_all_bots.py

--- run_all_bots.py
+++ run_all_bots.py
@@ -1,6 +1,6 @@
 import subprocess
 
-bots = ["1botaudio.py", "bot2.py", "bot3.py"]  # Adicione aqui os nomes dos seus scripts de bots
+bots = ["1botaudio.py"]  # Adicione aqui os nomes dos seus scripts de bots
 
 for bot in bots:
     subprocess.Popen(["python3", bot])
