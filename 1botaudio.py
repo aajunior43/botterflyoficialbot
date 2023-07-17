2ca3c6b99900d554a53a17d3b5baf1966f050a81
ALEKSANDRO ALVES DA ROCHA JUNIOR <97574073+aajunior43@users.noreply.github.com>
2023-07-17 19:13:54 -0300

    Update 1botaudio.py

--- 1botaudio.py
+++ 1botaudio.py
@@ -1,21 +1,11 @@
-import logging
 from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, ContextTypes
 from gtts import gTTS
 import os
 
-# Ative o logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+TOKEN = "seu_token_aqui"
 
-logger = logging.getLogger(__name__)
-
-# Token do Bot do Telegram
-TOKEN = "6081911245:AAFXOtmqUIT18EVXiGPcfoth7C3VKDl_0ic"
-
-def start(update: Update, _: CallbackContext) -> None:
-    """Envia uma mensagem quando o comando /start é emitido."""
+def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Olá {user.mention_markdown_v2()}\!',
@@ -23,7 +13,6 @@ def start(update: Update, _: CallbackContext) -> None:
     )
 
 def audio(update: Update, context: CallbackContext) -> None:
-    """Cria um arquivo de áudio a partir do texto enviado e o envia."""
     text = ' '.join(context.args)
     tts = gTTS(text=text, lang='pt')
     audio_file = 'text_to_audio.mp3'
@@ -31,23 +20,24 @@ def audio(update: Update, context: CallbackContext) -> None:
     with open(audio_file, 'rb') as f:
         context.bot.send_audio(chat_id=update.effective_chat.id, audio=f)
     os.remove(audio_file)
+    
+    try:
+        context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+    except Exception as e:
+        print(f"Erro ao apagar a mensagem: {e}")
 
 def main() -> None:
-    """Inicie o bot."""
-    updater = Updater(token=TOKEN)
+    updater = Updater(token=TOKEN, use_context=True)
 
-    # Obtenha o despachante para registrar manipuladores
     dispatcher = updater.dispatcher
 
-    # Comandos
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("audio", audio))
 
-    # Inicie o Bot
     updater.start_polling()
 
-    # Bloqueie até que você interrompa o bot (por exemplo, quando pressiona Ctrl + C em sua linha de comando).
     updater.idle()
 
 if __name__ == '__main__':
     main()
+    
